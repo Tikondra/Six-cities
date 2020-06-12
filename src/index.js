95b33ed705958001bf9cae7eeff7034981517914
Tikondra <tikondra47@gmail.com>
2020-06-12 11:58:55 +0400

    Добавляет PropTypes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app.jsx";
-import {Settings} from "./constants";
+import {PLACES, Settings} from "./constants";
 
 const init = () => {
   ReactDOM.render(
       <App
         placesCount = {Settings.COUNT_PLACES}
+        places = {PLACES}
       />,
       document.querySelector(`#root`)
   );
