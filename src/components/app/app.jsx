9e08e6bf64ca3392207d877d6a284d8afa4d16b5
Tikondra <tikondra47@gmail.com>
2020-06-20 19:06:31 +0400

    Добавляет react-router, и компонент Property

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 import PageMain from "../page-main/page-main.jsx";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import Property from "../property/property.jsx";
 
 const placeHeaderHandler = () => {};
 
 const App = (props) => {
   const {offers} = props;
 
-  return <PageMain
-    offers = {offers}
-    onClickByHeader = {placeHeaderHandler}
-  />;
+  return <BrowserRouter>
+    <Switch>
+      <Route exact path="/">
+        <PageMain
+          offers = {offers}
+          onClickByHeader = {placeHeaderHandler}
+        />
+      </Route>
+      <Route exact path="/dev-property">
+        <Property/>
+      </Route>
+    </Switch>
+  </BrowserRouter>;
 };
 
 App.propTypes = {
